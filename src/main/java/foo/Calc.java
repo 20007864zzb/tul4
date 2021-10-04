0d5d82342768ed97572451a29695cc304a9694a4
20007864zzb <91869308+20007864zzb@users.noreply.github.com>
2021-10-04 17:39:18 +0800

    Add files via upload

---
+++ src/main/java/foo/Calc.java
@@ -0,0 +1,16 @@
+package foo;
+
+public class Calc {
+public int add(int a,int b){
+    int c;
+    c=a+b;
+    System.out.println(c);
+    return c;
+}
+public int subtract(int a,int b){
+    int c;
+    c=a+b;
+    System.out.print(c);
+    return (c);
+}
+}
