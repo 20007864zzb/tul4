f823b23ec6c9ac6c29071b2b7df631c2eaf59cb4
20007864zzb <91869308+20007864zzb@users.noreply.github.com>
2021-10-04 16:37:45 +0800

    Add files via upload

---
+++ junit5sample/src/test/java/foo/AppTest.java
@@ -0,0 +1,24 @@
+package foo;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+
+{
+    /**
+     * Create the test case
+     *
+     */
+    @Test
+    public void appTest()
+    {
+        assertEquals(2, 2);
+    }
+
+
+}
